c87ebcd34af26cc5ffa2f8916f1528d1b0147f8b
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2024-02-29 18:19:18 +0100

    Add terms & conditions

---
+++ src/routes/terms-and-conditions/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Header from '../../lib/Header.svelte';
+	import Paragraph from '../../lib/Paragraph.svelte';
+	import Section from '../../lib/Section.svelte';
+</script>
+
+<Section>
+	<Header>Terms & Conditions</Header>
+	<Paragraph
+		>I agree to receive information electronically. The processing of my personal data will be
+		carried out by the Thriving Individuals Foundation (data controller) in the course of
+		maintaining regular contact with the organization in connection with its statutory activities,
+		in particular by sending free materials and informing about Thriving Individuals' actions and
+		opportunities to support the organization's activities.</Paragraph
+	>
+	<Paragraph
+		>I consent to the processing of my personal data by the Thriving Individuals Foundation (data
+		controller) for marketing purposes and to send commercial information by electronic means.</Paragraph
+	>
+</Section>
