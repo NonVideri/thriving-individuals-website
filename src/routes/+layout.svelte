6fee76c6d7a95e9377c1a0b049562c1b9c2b98a3
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2024-09-07 22:27:39 +0200

    Add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,3 +9,9 @@
 <div class="flex flex-col items-center w-full">
 	<slot />
 </div>
+<footer
+	class="flex flex-col items-center w-full mt-32 md:mt-40 xl:mt-60 py-4 text-sm text-gray-500"
+>
+	<p>© 2021 Thriving Individuals Foundation</p>
+	<p>All rights reserved.</p>
+</footer>
