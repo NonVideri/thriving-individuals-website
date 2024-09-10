697667f2a9c3529cb09099c35846475f61de57f8
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2024-09-10 17:46:47 +0200

    Improve and extend Section, remove Subsection

--- src/lib/Section.svelte
+++ src/lib/Section.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { SECTION_CLASSES } from './types';
+	import { SECTION_CLASSES } from './classes';
 	import type { ClassValue } from 'clsx';
 	import { cn } from './utils';
 
 	export let id = '';
 	let className: ClassValue = undefined;
 	export { className as class };
+	export let isDark = false;
 	let section: HTMLElement;
-	let isVisible = false;
+	export let isVisible = false;
 
 	onMount(() => {
 		let observer = new IntersectionObserver(
@@ -16,12 +17,13 @@
 				entries.forEach((entry) => {
 					if (entry.isIntersecting && entry.intersectionRatio > 0.5) {
 						isVisible = true;
-					} else if (
-						!entry.isIntersecting ||
-						(entry.isIntersecting && entry.intersectionRatio <= 0.3)
-					) {
-						isVisible = false;
 					}
+					// else if (
+					// 	!entry.isIntersecting ||
+					// 	(entry.isIntersecting && entry.intersectionRatio <= 0.3)
+					// ) {
+					// 	isVisible = false;
+					// }
 				});
 			},
 			{
@@ -37,11 +39,18 @@
 	bind:this={section}
 	{id}
 	class={cn(
-		SECTION_CLASSES,
-		'transform translate-y-5 mt-20 md:mt-32 xl:mt-40 px-20 md:px-40 xl:px-64 2xl:px-96',
-		isVisible ? 'opacity-100 animate-fadeIn' : 'opacity-0',
-		className
+		'w-full px-6 md:px-10 xl:px-16 2xl:px-24 mb-6 md:mb-10 xl:mb-16 2xl:mb-24',
+		isVisible ? 'opacity-100 animate-fadeIn' : 'opacity-0'
 	)}
 >
-	<slot />
+	<div
+		class={cn(
+			'py-12 md:py-16 xl:py-24 px-16 sm:px-16 md:px-20 lg:px-40 xl:px-64 2xl:px-80',
+			isDark ? 'bg-surface-800 text-white' : 'bg-primary-600 text-black',
+			SECTION_CLASSES,
+			className
+		)}
+	>
+		<slot />
+	</div>
 </section>
