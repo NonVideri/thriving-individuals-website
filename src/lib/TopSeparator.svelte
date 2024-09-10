b6934796e7523b0a7963b2df92b91fc99f0956a4
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2024-09-10 17:46:24 +0200

    Add TopSeparator component

---
+++ src/lib/TopSeparator.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { cn } from './utils';
+
+	export let isDark = false;
+</script>
+
+<div class={cn('pt-6 md:pt-10 xl:pt-12 2xl:pt-20 w-full', isDark && 'bg-surface-800')} />
