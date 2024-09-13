2ac5778be1ec64abf3e55396dc1ac34f19d699da
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2024-09-13 22:08:28 +0200

    Add Subscribe modal

--- src/lib/SubscribeForm.svelte
+++ src/lib/SubscribeForm.svelte
@@ -6,6 +6,10 @@
 	import Paragraph from './Paragraph.svelte';
 	import Input from './Input.svelte';
 	import Link from './Link.svelte';
+	import Header from './Header.svelte';
+	import { getModalStore } from '@skeletonlabs/skeleton';
+
+	const modalStore = getModalStore();
 
 	const handleSubmit: SubmitFunction = ({ formData, cancel }) => {
 		if (!formData.get('accepted')) {
@@ -16,6 +20,8 @@
 
 		return ({ result, update }) => {
 			if (result.type === 'success') {
+				localStorage.setItem('subscribed', 'true');
+				modalStore.close();
 				setAlert('Almost there! Please check your email to confirm the subscription.', 'success');
 				return update();
 			}
@@ -30,19 +36,32 @@
 			}
 		};
 	};
+
+	const handleClose = () => {
+		modalStore.close();
+		localStorage.setItem('subscribed', 'true');
+	};
 </script>
 
 <form
-	class="flex flex-col space-y-8 w-3/5 max-w-md my-10"
+	class="absolute flex flex-col space-y-5 md:space-y-6 lg:space-y-7 xl:space-y-8 pt-8 pb-10 px-32 max-w-2xl rounded-xl bg-primary-600"
 	method="POST"
 	action="?/subscribe"
 	use:enhance={handleSubmit}
 >
+	<button
+		type="button"
+		class="absolute top-2 right-5 text-3xl text-black cursor-pointer"
+		on:click={handleClose}
+	>
+		&times;
+	</button>
+	<Header type="h4" class="self-center">Join our elite mailing list</Header>
 	<Input type="text" name="name" placeholder="Name *" required />
 	<Input type="email" name="email" placeholder="Email *" required />
 
 	<div class="checkbox-container">
-		<Paragraph>
+		<Paragraph class="my-0 py-0">
 			<label class="flex items-center">
 				<input type="checkbox" name="accepted" class="w-5 h-5 mr-4" />
 				Accept&nbsp;<Link href="/terms-and-conditions">Terms & Conditions</Link>
@@ -56,7 +75,7 @@
 	>
 		Subscribe
 	</button>
+	<Paragraph type="small">
+		We only send occasional emails with the most important updates. We will never spam you.
+	</Paragraph>
 </form>
-<Paragraph type="small">
-	We only send occasional newsletters with the most important updates. We will never spam you.
-</Paragraph>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,41 @@
 <script lang="ts">
+	import {
+		getModalStore,
+		initializeStores,
+		Modal,
+		type ModalComponent,
+	} from '@skeletonlabs/skeleton';
 	import '../app.pcss';
+	import SubscribeForm from '$lib/SubscribeForm.svelte';
 	import Alert from './Alert.svelte';
 	import NavBar from './NavBar.svelte';
+	import { onMount } from 'svelte';
+
+	const modalComponent: ModalComponent = { ref: SubscribeForm };
+
+	initializeStores();
+
+	const modalStore = getModalStore();
+
+	onMount(() => {
+		if (!localStorage.getItem('subscribed')) {
+			modalStore.trigger({
+				type: 'component',
+				component: modalComponent,
+			});
+		}
+	});
 </script>
 
 <NavBar />
 <Alert />
+<Modal />
 <div
 	class="flex flex-col items-center w-full mt-12 sm:mt-[50px] md:mt-[52px] lg:mt-[60px] xl:mt-20"
 >
 	<slot />
 </div>
 <footer class="flex flex-col items-center w-full py-4 md:py-6 xl:py-8 text-sm bg-primary-600">
-	<p>© 2021 Thriving Individuals Foundation</p>
+	<p>© 2023 Thriving Individuals Foundation</p>
 	<p>All rights reserved.</p>
 </footer>
