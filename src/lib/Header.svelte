eb5a288862a89ccb4597070295c13507d55d4eaf
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2024-09-13 08:38:07 +0200

    Make Header and Paragraph more responsive

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -9,23 +9,53 @@
 </script>
 
 {#if type === 'h1'}
-	<h1 class={cn('text-3xl md:text-4xl xl:text-5xl 2xl:text-6xl mb-6', HEADER_CLASSES, className)}>
+	<h1
+		class={cn(
+			'text-3xl md:text-4xl xl:text-5xl 2xl:text-6xl mb-3 md:mb-6',
+			HEADER_CLASSES,
+			className
+		)}
+	>
 		<slot />
 	</h1>
 {:else if type === 'h2'}
-	<h2 class={cn('text-xl md:text-2xl xl:text-3xl 2xl:text-4xl mb-5', HEADER_CLASSES, className)}>
+	<h2
+		class={cn(
+			'text-xl md:text-2xl xl:text-3xl 2xl:text-4xl mb-2 md:mb-5',
+			HEADER_CLASSES,
+			className
+		)}
+	>
 		<slot />
 	</h2>
 {:else if type === 'h3'}
-	<h3 class={cn('text-lg md:text-xl xl:text-2xl 2xl:text-3xl mb-3', HEADER_CLASSES, className)}>
+	<h3
+		class={cn(
+			'text-lg md:text-xl xl:text-2xl 2xl:text-3xl mb-1 md:mb-3',
+			HEADER_CLASSES,
+			className
+		)}
+	>
 		<slot />
 	</h3>
 {:else if type === 'h4'}
-	<h4 class={cn('text-base md:text-lg xl:text-xl 2xl:text-2xl mb-2', HEADER_CLASSES, className)}>
+	<h4
+		class={cn(
+			'text-base md:text-lg xl:text-xl 2xl:text-2xl mb-1 md:mb-2',
+			HEADER_CLASSES,
+			className
+		)}
+	>
 		<slot />
 	</h4>
 {:else if type === 'h5'}
-	<h5 class={cn('text-sm md:text-base xl:text-lg 2xl:text-xl mb-1', HEADER_CLASSES, className)}>
+	<h5
+		class={cn(
+			'text-sm md:text-base xl:text-lg 2xl:text-xl mb-0.5 md:mb-1',
+			HEADER_CLASSES,
+			className
+		)}
+	>
 		<slot />
 	</h5>
 {:else if type === 'h6'}
