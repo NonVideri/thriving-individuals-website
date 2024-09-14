627b5f0615edbfaef32030ec27691ef174eaf853
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2024-09-14 15:28:25 +0200

    Revamp the landing page texts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,71 +4,94 @@
 	import Paragraph from '$lib/Paragraph.svelte';
 	import List from '$lib/List.svelte';
 	import Link from '$lib/Link.svelte';
-
-	const VALUES = [
-		{
-			title: 'Freedom',
-			description:
-				'We respect freedom as a fundamental aspect of human nature. We strive to live and act as \
-        free men, both internally and externally, making the world more free one person at a time.',
-		},
-		{
-			title: 'Responsibility',
-			description:
-				'We proudly take ownership of our lives, families, businesses, and communities, to the degree \
-        that we are capable of, shaping our fates and the fate of the world around us for the better.',
-		},
-		{
-			title: 'Excellence',
-			description:
-				'We are not satisfied with mediocrity, but strive to grow and get better at who we are and what \
-        we do every single day, extending our being and influence in the ways that we choose.',
-		},
-		{
-			title: 'Empowerment',
-			description:
-				'We generously share the gift of our growth and freedom with others, who are able and willing to \
-        walk a similar path of self-improvement and personal independence.',
-		},
-	];
 </script>
 
 <img id="home" src="/logo.png" alt="Thriving Individuals Logo" class="animate-fadeIn w-[90%]" />
 
-<Section id="about" class="mt-4 md:mt-6 xl:mt-8">
+<Section class="mt-4 md:mt-6 xl:mt-8">
 	<Header>About Us</Header>
 	<Paragraph>
 		We are a global community of freedom-loving individuals, who are dedicated to personal growth,
-		and the pursuit of excellence in all aspects of life.
+		and the pursuit of excellence in all aspects of our lives.
 	</Paragraph>
 	<Paragraph>
-		We empower free people all around the world to achieve success in their personal lives, and live
-		them to the fullest, in freedom and independence.
+		We empower free people all around the world to achieve fulfillment in their personal lives, and
+		success in their careers & businesses.
+	</Paragraph>
+	<Paragraph>
+		We embrace all aspects of human flourishing and the pursuit of virtue, growth, and personal
+		excellence.
 	</Paragraph>
 	<Paragraph>
 		Interested? <Link href="/join">Join us</Link>.
 	</Paragraph>
 </Section>
 
-<Section id="mission" isDark>
-	<Header>Our Mission</Header>
-	<Paragraph>
-		We don't practice liberty activism, but we empower the ones who do.<br />
-		Teaching free people how to be more powerful and self-reliant as individuals,<br />
-		and creating sustainable foundations for free communities that can resist tyranny –<br />
-		this is the mission of the Thriving Individuals.
-	</Paragraph>
-</Section>
-
-<Section id="vision">
+<Section isDark>
 	<Header>Our Vision</Header>
-	<Paragraph>
-		We embrace all aspects of human flourishing and the human pursuit of virtue, growth, and
-		personal excellence.
-	</Paragraph>
+	<List
+		items={[
+			{
+				title: 'Thriving Individuals',
+				description:
+					'Free people so powerful and influential, that they are impossible to enslave, and are allowed to unleash their full potential.',
+			},
+			{
+				title: 'Thriving Communities',
+				description:
+					'Free communities so self-reliant and prosperous, that they are impossible to control, and are allowed to flourish in peace.',
+			},
+		]}
+	/>
 </Section>
 
-<Section id="values" isDark>
-	<Header>Our Values — FREE</Header>
-	<List items={VALUES} />
+<Section>
+	<Header>Our Mission</Header>
+	<Paragraph>We make the world more free one person, and one community at a time.</Paragraph>
+	<List
+		items={[
+			{
+				title: 'Empowering Free Individuals',
+				description:
+					'We help free people grow, flourish, and become more powerful, influential, and self-reliant.',
+			},
+			{
+				title: 'Empowering Free Communities',
+				description:
+					'We create sustainable foundations for free communities that can resist tyranny.',
+			},
+		]}
+	/>
+</Section>
+
+<Section isDark>
+	<Header>Our Values</Header>
+	<List
+		items={[
+			{
+				title: 'Freedom',
+				description:
+					'We embrace inner and outer freedom as an important aspect of human nature, and strive to peacefully live \
+          and let live as free men and women.',
+			},
+			{
+				title: 'Responsibility',
+				description:
+					'We take ownership of our lives, families, businesses, and communities, shaping our lives and the lives \
+          of people close to us for the better.',
+			},
+			{
+				title: 'Excellence',
+				description:
+					'We strive to grow and get better at who we are and what we do every single day, extending \
+          our being and sphere of influence, and not being satisfied with mediocrity.',
+			},
+			{
+				title: 'Empowerment',
+				description:
+					'We generously share the gift of our growth and freedom with others, who are able and willing to \
+          walk a similar path of self-improvement and personal independence.',
+			},
+		]}
+	/>
 </Section>
