5af905eadb0e43c3f0ec3b207cf48fb3fcbb7102
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2024-09-28 22:46:32 +0200

    Add some route lists to constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,17 +3,37 @@ export const THRIVING_INDIVIDUALS_REGON = '52812300100000';
 export const THRIVING_INDIVIDUALS_IBAN = 'PL 50 2530 0008 2060 1081 4632 0001';
 
 export const Routes = {
-  CONTACT: '/contact',
-  DONATE: '/donate',
-  FRAMEWORK: '/framework',
-  HOME: '/',
-  JOIN: '/join',
-  LEGAL: '/legal',
-  PARTNERSHIPS: '/partnerships',
-  PROJECTS: '/projects',
-  PROJECTS_JOINT: '/projects/joint',
-  STATUTE: '/statute',
-  SUBSCRIBE: '/subscribe',
-  TEAM: '/team',
-  TERMS_AND_CONDITIONS: '/terms-and-conditions',
+	API_CONTACT: '/api/contact',
+	CONTACT: '/contact',
+	DONATE: '/donate',
+	FRAMEWORK: '/framework',
+	HOME: '/',
+	JOIN: '/join',
+	LEGAL: '/legal',
+	PARTNERSHIPS: '/partnerships',
+	PROJECTS: '/projects',
+	PROJECTS_DEEP_TALKS: '/projects/deep-talks',
+	PROJECTS_JOINT: '/projects/joint',
+	PROJECTS_SELF_IMPROVEMENT_MEETUP: '/projects/self-improvement-meetup',
+	STATUTE: '/statute',
+	SUBSCRIBE: '/subscribe',
+	TEAM: '/team',
+	TERMS_AND_CONDITIONS: '/terms-and-conditions',
 };
+
+export const NO_SEPARATOR_ROUTES = [Routes.HOME, Routes.STATUTE];
+
+export const PROJECTS = [
+	{
+		text: 'Self-Improvement Meetup',
+		href: Routes.PROJECTS_SELF_IMPROVEMENT_MEETUP,
+	},
+	{
+		text: 'Deep Talks',
+		href: Routes.PROJECTS_DEEP_TALKS,
+	},
+	{
+		text: 'Joint projects',
+		href: Routes.PROJECTS_JOINT,
+	},
+];
